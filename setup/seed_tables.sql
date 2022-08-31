121c6f3be0a8c215638af3d5d61a0e758d361cc6
Hailey Eckstrand <hailey@foundryspatial.com>
2022-08-31 16:22:56 -0700

    Put asset insert into the wrong sql file

--- setup/seed_tables.sql
+++ setup/seed_tables.sql
@@ -10,3 +10,21 @@ INSERT INTO data.parents (parent_name, user_id, risk_level_threshold) VALUES
 ('Infrastructures', 2, 1),    -- City of Merritt:           2
 ('Infrastructures', 3, 1),    -- City of Abbotsford:        3
 ('Pipelines Cluster 1', 4, 3); --Trans Mountain Corporation 4
+
+INSERT INTO data.assets (
+	asset_name, 
+	asset_description, 
+	parent_id, 
+	latitude, 
+	longitude, 
+	watershed_feature_id, 
+	aoi_geom4326,
+	aoi_area_m2,
+	land_disturbance_fire, -- 0 | 1 | 2 -- trigger
+	land_disturbance_road, -- 0 | 1 | 2 -- trigger
+	fire_past_2_years_m2,
+	fire_past_5_years_m2,
+	length_of_roads_m
+) VALUES
+
+('Section 2', 'Coquihalla Summit to Merritt', 1, 49.5941, -121.0995, 10644179, ST_Multi(ST_Buffer(ST_SetSRID(ST_Point(-121.0995, 49.5941), 4326), 0.001)), ST_Area(ST_Buffer(ST_SetSRID(ST_Point(-121.0995, 49.5941), 4326), 0.001)::geography), 0, 0, 1000, 2000, 300); -- BC MOTI asset
