f8c581857db9f28e6c3fb99664accb770dbcca48
Christopher Webb <49045559+chrwebb@users.noreply.github.com>
2022-09-07 15:22:04 -0700

    Update create_tables_and_views.sql

--- setup/create_tables_and_views.sql
+++ setup/create_tables_and_views.sql
@@ -20,16 +20,6 @@ CREATE TABLE data.models (
 	model_name_long text  NOT NULL --Add more metadata fields later and populate
 );
 
-
-DROP TABLE IF EXISTS data.risk_levels CASCADE;
-CREATE TABLE data.risk_levels (
-	risk_level INTEGER NOT NULL,
-	lower_bound double precision,
-	upper_bound double precision,
-	risk_label text NOT NULL,
-	return_period text NOT NULL
-);
-
 DROP TABLE IF EXISTS data.networks CASCADE;
 CREATE TABLE data.networks
 (
