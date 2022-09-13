5b7e2e0739fc467586fb4820037b9d6744601054
Chris Webb <cwebb@foundryspatial.com>
2022-09-13 14:40:51 -0700

    FWA seed data

--- setup_seed_data.sh
+++ setup_seed_data.sh
@@ -8,4 +8,9 @@ unzip -qun data/test/roads_4326.zip -d data/test/
 ogr2ogr -overwrite -f PostgreSQL PG:"host=$PGHOST user=$PGUSER dbname=$PGDATABASE" data/test/roads_4326.geojson -dialect sqlite -nln staging.road_lines -sql "select Geometry as geom4326 from roads_4326"
 rm data/test/roads_4326.geojson
 
+unzip -qun data/test/fwa_4326.zip -d data/test/
+ogr2ogr -overwrite -f PostgreSQL PG:"host=$PGHOST user=$PGUSER dbname=$PGDATABASE" data/test/fwa_4326.geojson -dialect sqlite -nln data.freshwater_atlas -sql "select Geometry as geom4326 from fwa_4326"
+rm data/test/fwa_4326.geojson
+
 psql postgres://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE -v ON_ERROR_STOP=1 -f setup/sql/seed_tables.sql
+
