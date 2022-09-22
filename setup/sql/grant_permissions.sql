279addf1a231156d9e5d4faece941ec740241f37
Chris W <cwebb@foundryspatial.com>
2022-09-22 13:32:23 -0700

    Tileserv functions for sentinel and asset by id

--- setup/sql/grant_permissions.sql
+++ setup/sql/grant_permissions.sql
@@ -2,6 +2,9 @@ ALTER FUNCTION data.fire_polygons_tiles_for_year(z integer, x integer, y integer
 ALTER FUNCTION data.watershed_polygon_tiles_for_asset(z integer, x integer, y integer, in_asset_id integer) SECURITY DEFINER SET search_path = public;
 ALTER FUNCTION data.asset_point_tiles_for_group(z integer, x integer, y integer, in_group_id integer) SECURITY DEFINER SET search_path = public;
 ALTER FUNCTION data.sentinel_point_tiles_for_group(z integer, x integer, y integer, in_group_id integer) SECURITY DEFINER SET search_path = public;
+ALTER FUNCTION data.asset_point_tiles_by_asset_id (z integer, x integer, y integer, in_asset_id integer) SECURITY DEFINER SET search_path = public;
+ALTER FUNCTION data.sentinel_point_tiles_by_sentinel_id (z integer, x integer, y integer, in_sentinel_id integer) SECURITY DEFINER SET search_path = public;
+
 
 GRANT USAGE ON SCHEMA data TO tileserver_user;
 
@@ -9,3 +12,5 @@ GRANT EXECUTE ON FUNCTION data.fire_polygons_tiles_for_year TO tileserver_user;
 GRANT EXECUTE ON FUNCTION data.watershed_polygon_tiles_for_asset TO tileserver_user;
 GRANT EXECUTE ON FUNCTION data.asset_point_tiles_for_group TO tileserver_user;
 GRANT EXECUTE ON FUNCTION data.sentinel_point_tiles_for_group TO tileserver_user;
+GRANT EXECUTE ON FUNCTION data.asset_point_tiles_by_asset_id TO tileserver_user;
+GRANT EXECUTE ON FUNCTION data.sentinel_point_tiles_by_sentinel_id TO tileserver_user;
