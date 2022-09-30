db747f94bd2cf4cba6c52445a7249726bf03fe4d
Sina <sina@foundryspatial.com>
2022-09-30 09:57:35 -0700

    worked on the structure of hierarchy of codes and finalized logging process for hrdps data

--- cron/src/forecasts/hrdps/et.py
+++ cron/src/forecasts/hrdps/et.py
@@ -5,39 +5,11 @@ from datetime import datetime
 import os
 import logging
 
-
-logger = logging.getLogger(__name__)
-## create console handler 
-c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler('file.log')
-
-## setting severity level
-## if we use WARNING it will only log warning and higher levels 
-## Debug, info, warning, error, critical - severity levels from low to high 
-c_handler.setLevel(logging.WARNING)
-f_handler.setLevel(logging.WARNING)
-
-## creating formatter for each handler 
-c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-## this means that format of console log and fole log are exactly the same
-## except for the time log on the file 
-
-## now we need to attach the formatting objects to each handler 
-
-c_handler.setFormatter(c_format)
-f_handler.setFormatter(f_format)
-
-## now we need to link the handlers with formatter to our custom logger 
-logger.addHandler(c_handler)
-logger.addHandler(f_handler)
-
-
-
 url = 'https://dd.alpha.weather.gc.ca/model_hrdps/west/1km/grib2/'
 nSub_folders = 5 ## for testing - we should make this 49 when doing the actual scraping 
-save_path = "scraped_hrdps_data/"
+save_path = "forecasts/hrdps/hrdps_data/"
 
+logger = logging.getLogger('forecasts')
 
 
 def downloadlinks(url, path):
@@ -55,7 +27,8 @@ def downloadlinks(url, path):
 			link = element.get('href')
 			response = requests.get(cur_url + link)
 			open(path + element.text, "wb").write(response.content)
-			logger.warning('downloaded file: {}'.format(link))
+			logger.debug('downloaded file: {}'.format(link))
+	
 		except Exception as er:
 			logger.error(str(er))
 
@@ -71,10 +44,5 @@ def process():
 	path_12 = path + "12/"
 
 	downloadlinks(url + "00/", path_0)
-	downloadlinks(url + "00/", path_12)
-
-
-
-
-process()
+	downloadlinks(url + "12/", path_12)
 
--- cron/src/utils/functions.py
+++ cron/src/utils/functions.py
@@ -7,3 +7,22 @@
 ## get all sentinels (geom+id) for iteration elsewehere
 
 ## insert record into forecast model table
+
+import logging
+import inspect
+import os
+log_file = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'logs/app.log'))
+
+
+def setup_logging():
+	logger = logging.getLogger('forecasts')
+	logger.setLevel(logging.DEBUG)
+	fh = logging.FileHandler(log_file)
+	fh.setLevel(logging.DEBUG)
+	ch = logging.StreamHandler()
+	ch.setLevel(logging.ERROR)
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(pathname)s - %(funcName)s - %(levelname)s - %(message)s')
+	fh.setFormatter(formatter)
+	ch.setFormatter(formatter)
+	logger.addHandler(fh)
+	logger.addHandler(ch)
